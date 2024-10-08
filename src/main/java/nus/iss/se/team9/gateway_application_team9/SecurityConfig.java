36beb03cf61906d9dbdb7d160a56eab716a4c348
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-10-08 16:36:04 +0800

    add cors

--- src/main/java/nus/iss/se/team9/gateway_application_team9/SecurityConfig.java
+++ src/main/java/nus/iss/se/team9/gateway_application_team9/SecurityConfig.java
@@ -1,23 +1,23 @@
-package nus.iss.se.team9.gateway_application_team9;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
-        http
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/api/auth/login").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
-        return http.build();
-    }
-}
+//package nus.iss.se.team9.gateway_application_team9;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//    @BeanWebConfig
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+//        http
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/api/auth/login").permitAll()
+//                        .anyExchange().authenticated()
+//                )
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+//        return http.build();
+//    }
+//}
